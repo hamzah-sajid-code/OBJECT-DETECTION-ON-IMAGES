e93c5f36385879d7cf67a61f673a93103c6b1a5a
hamzah-sajid-code <82176013+hamzah-sajid-code@users.noreply.github.com>
2021-08-19 18:08:12 +0300

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,28 @@
+var img = '';
+var status = '';
+var arrayOfResults = [];
+
+function preload() {
+    img = loadImage('https://i.postimg.cc/tTsNsn1G/bottle.jpg');
+}
+function setup() {
+    canvas = createCanvas(640, 420)
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+}
+
+function modelLoaded() {
+    console.log('Successfully Loaded Coco SSD');
+    status = true;
+    document.getElementById('status').innerHTML = "Status: Detected The Object";
+    objectDetector.detect(img, gotResults);
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+
+function gotResults(results) {
+    console.log(results);
+    arrayOfResults = results;
+    console.log(arrayOfResults)
+}
---
+++ tv.js
@@ -0,0 +1,29 @@
+var img = '';
+var status = '';
+var arrayOfResults = [];
+
+function preload() {
+    img = loadImage('https://i.postimg.cc/mrtHjfTQ/tv.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420)
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+}
+
+function modelLoaded() {
+    console.log('Successfully Loaded Coco SSD');
+    status = true;
+    document.getElementById('status').innerHTML = "Status: Detected The Object";
+    objectDetector.detect(img, gotResults);
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+
+function gotResults(results) {
+    console.log(results);
+    arrayOfResults = results;
+    console.log(arrayOfResults)
+}
